6160a90b16a7f4a8ec4cdc433a627f23045acceb
hamada <bhak6150@gmail.com>
2025-01-16 22:25:30 +0100

    updateUser

--- src/compenents/Users/Details.jsx
+++ src/compenents/Users/Details.jsx
@@ -3,13 +3,11 @@ import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
 
 
 export default function Details() {
-
-
-
-
+    const navigate = useNavigate();
     const { id } = useParams();
     const { register, handleSubmit, setValue } = useForm();
 
@@ -20,26 +18,33 @@ export default function Details() {
         setValue("phone", data.user.phone);
         console.log(data)
     }
-
-
-
     useEffect(() => {
         getDetails();
     }, [])
 
+    const updateUser = async (value) => {
 
+        const response = await axios.put(`${import.meta.env.VITE_BURL}/users/${id}`,
 
+            {
+                userName: value.userName
+            }
+
+        );
+
+        if (response.status === 200) {
+
+            navigate('/users');
+            alert("User updated successfully");
+        }
+    }
 
     return (
         <>
-
             <div className=" container vh-100   ">
-
                 <div className="row">
-
-
                     <h1>Create User </h1>
-                    <form onSubmit={handleSubmit()}>
+                    <form onSubmit={handleSubmit(updateUser)}>
 
                         <div className="form-floating mb-3">
                             <input type="text" className="form-control" id="userName" placeholder=""  {...register("userName")} />
@@ -51,24 +56,14 @@ export default function Details() {
                             <label htmlFor="email">Email address</label>
 
                         </div>
-
-
-
                         <div className="form-floating mb-3">
                             <input type="text" className="form-control" id="phone" placeholder="+01000000"  {...register("phone")} />
                             <label htmlFor="phone">phone Number </label>
                         </div>
-
-
                         <button type="submet" className="btn btn-outline-primary">Update</button>
                     </form>
-
                 </div>
             </div>
-
-
-
-
         </>
     )
 }
